a60b3f58f5c94a30a50f7b0f6b0f0617f4d090ef
gremlin079 <sashdmitriev2006@gmail.com>
2024-11-24 14:01:30 +0300

    Добавить функции для подсчета времени

--- time_functions.cpp
+++ time_functions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -18,12 +19,61 @@ short dateToMonth(string date)
 	return month;
 }
 
-int timeToMinute(int time)
+int timeToMinute(vector<int> time)
 {
-	return 0;
+	int start_minutes = (time[0] * 60) + time[1];
+	int end_minutes = (time[2] * 60) + time[3];
+
+	if (start_minutes <= end_minutes)
+	{
+		if ((end_minutes - start_minutes) > 240)
+		{
+			return end_minutes - start_minutes - 45;
+		}
+		else
+		{
+			return end_minutes - start_minutes;
+		}
+
+	}
+
+	else
+	{
+		if (((1440 - start_minutes) + end_minutes) > 240)
+		{
+			return (1440 - start_minutes) + end_minutes - 45;
+		}
+		else
+		{
+			return (1440 - start_minutes) + end_minutes;
+		}
+
+	}
 }
 
-int minuteToDDHHMM(int time)
+string minuteToDDHHMM(int time)
 {
-	return 0;
+	int work_in_hours = 0;
+	int work_in_minutes = 0;
+	int work_in_days = 0;
+	
+	std::string result;
+
+	if (time >= 1440)
+	{
+		work_in_days = time / 1440;
+		work_in_hours = (time - (work_in_days * 1440)) / 60;
+		work_in_minutes = (time - (work_in_days * 1440) - (work_in_hours * 60));
+	}
+	
+
+	else
+	{
+		work_in_hours = time / 60;
+		work_in_minutes = (time - (work_in_hours * 60));
+	}
+
+	result = std::to_string(work_in_days) + ":" + std::to_string(work_in_hours) + ":" + std::to_string(work_in_minutes);
+	
+	return result;
 }
